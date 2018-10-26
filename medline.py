49de183d5aef8d385d46de6ceb683a1eaa418b6c
manos89 <mvahlas@gmail.com>
2018-10-26 16:42:13 +0200

    second

--- medline.py
+++ medline.py
@@ -12,22 +12,22 @@ db = client['large_papers']
 text=open('Medline.txt','wb')
 results=db['patents'].find()
 for r in results:
-	brief_text=''
-	patent_id=str(r['id'])
-	title='TI  - '+str(r['title']).strip().lstrip()
-	abstract='AB  - '+r['abstract'].strip().lstrip()
-	brief_results=db['brf_sum_text'].find_one({'patent_id':patent_id})
-	if brief_results:
-		brief_text=' '+str(brief_results['text'])
-		print(brief_text)
-	patent_id_write='PMID- '+patent_id
-	full_text=abstract.strip().lstrip().rstrip()+' '+brief_text.strip().lstrip().rstrip()
-	text_to_write=patent_id_write.strip().lstrip().rstrip()+'\n'+title.strip().lstrip().rstrip()+'\n'+full_text.strip()
-	text.write(text_to_write[:2500].encode('ascii')+'\n\n'.encode('ascii'))
-	if count%10==0:
-		print(count)
-		quit()
-	count+=1
+	try:
+		brief_text=''
+		patent_id=str(r['id'])
+		title='TI  - '+str(r['title']).strip().lstrip()
+		abstract='AB  - '+r['abstract'].strip().lstrip()
+		brief_results=db['brf_sum_text'].find_one({'patent_id':patent_id})
+		if brief_results:
+			brief_text=' '+str(brief_results['text'])
+			print(brief_text)
+		patent_id_write='PMID- '+patent_id
+		full_text=abstract.strip().lstrip().rstrip()+' '+brief_text.strip().lstrip().rstrip()
+		text_to_write=patent_id_write.strip().lstrip().rstrip()+'\n'+title.strip().lstrip().rstrip()+'\n'+full_text.strip()
+		text.write(text_to_write[:2500].encode('ascii')+'\n\n'.encode('ascii'))
+		count+=1
+	except Exception as E:
+		print(str(E))
 
 text.close()
 
